f78201fb1f790800d742dc53fc0f9bd5c246946a
tomas <twatson@frba.utn.edu.ar>
2022-05-05 15:15:59 -0300

    Subir correcciones

--- Pdep-Commerce/src/Lib.hs
+++ Pdep-Commerce/src/Lib.hs
@@ -17,7 +17,7 @@ entregaSencilla :: String -> Bool
 entregaSencilla fecha = (even . length) fecha
 
 descodiciarProducto :: Producto -> String
-descodiciarProducto unProducto = take 10 (nombre unProducto)
+descodiciarProducto unProducto = (take 10) . nombre $ unProducto
 
 productoDeLujo :: Producto -> Bool
 productoDeLujo unProducto = (elem 'x' (nombre unProducto)) || (elem 'z' (nombre unProducto))
@@ -29,7 +29,7 @@ aplicarCostoDeEnvio :: Num a => a -> a -> a
 aplicarCostoDeEnvio precio costoEnvio = precio + costoEnvio
 
 productoCodiciado :: Producto -> Bool
-productoCodiciado unProducto = esMayorA10 (length . nombre $ unProducto)
+productoCodiciado unProducto = esMayorA10 . length . nombre $ unProducto
 
 esMayorA10 :: Int -> Bool
 esMayorA10 longitud = longitud > 10
@@ -44,7 +44,7 @@ productoXL :: Producto -> String
 productoXL unProducto = ((++) . nombre $ unProducto) "XL"
 
 versionBarata :: Producto -> String
-versionBarata unProducto = reverse (descodiciarProducto unProducto)
+versionBarata unProducto = reverse . descodiciarProducto $ unProducto
 
 
 {-
